36cbb52b8373a36de5183438bd4386dfb280c143
AllardWijmenga <ra.wijmenga@gmail.com>
2023-04-18 09:50:05 +0200

    contact pagina updates

--- contact.php
+++ contact.php
@@ -12,18 +12,19 @@
 </head>
 <body>
     <!-- input to get user data  -->
-<form action="contact.php" method="post">
-    <label for="Naam">Uw Naam :</label>
-    <input type="name" name="name" id="name">
-    <label for="Email">Uw email :</label>
-    <input type="email" name="Email" id="Email">
-    <label for="Telefoon">Telefoon :</label>
-    <input type="Telefoon" name="Telefoon" id="Telefoon">
-    <label for="Opmerking">Opmerking :</label>
-    <input type="Opmerking" name="Opmerking" id="Opmerking">
-    <input type="submit" value="Submit">
-    </form>
-
+    <div id="contact-form">
+        <form action="contact.php" method="post">
+        <label for="Naam">Uw Naam :</label>
+        <input type="name" name="name" id="name">
+        <label for="Email">Uw email :</label>
+        <input type="email" name="Email" id="Email">
+        <label for="Telefoon">Telefoon :</label>
+        <input type="Telefoon" name="Telefoon" id="Telefoon">
+        <label for="Opmerking">Opmerking :</label>
+        <input type="Opmerking" name="Opmerking" id="Opmerking">
+        <input type="submit" value="Submit">
+        </form>
+    </div>
 <?php 
     // running the function if the users email is set
     if(isset($_POST['Email']))
