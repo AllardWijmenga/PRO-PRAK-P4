31a0c46df4fb84fdaf70f1a82e25a0b1feb4a4e1
Axel van_beek <336019@student.mboutrecht.nl>
2023-05-09 09:06:35 +0200

    testing the cms system with different html elements

--- index.php
+++ index.php
@@ -26,14 +26,14 @@
 
   <div class="container">
     <div class="columnhp1">
-      <h1>Betrouwbaar internet voor reizigers met Starlink</h1>
-      <p> Reis jij graag? Met Starlink's Portability add-on en Roam heb je overal toegang tot snel en betrouwbaar internet met lage latency. Neem je Starlink-apparatuur gemakkelijk mee en pauzeer en betaal alleen voor de periode waarin je internet nodig hebt met Roam. Ideaal voor werken op afstand, streamen en gamen tijdens het reizen.</p>
-      <img src="../img/Homepagina/camper.jpg" alt="img1">
+      <h1><?php $result = GetDBInfo('Info1' , 2); foreach($result as $info){echo "$info->Info1";}?></h1>
+      <p> <?php $result = GetDBInfo('Info1' , 3); foreach($result as $info){echo "$info->Info1";}?></p>
+      <img src="img/Homepagina/camper.jpg" alt="img1">
     </div>
     <div class="columnhp2">
       <h1>Flexibiliteit</h1>
       <p>Starlink biedt flexibiliteit en eerlijke voorwaarden voor hun klanten door geen langetermijncontracten aan te bieden en prijzen naar behoefte aan te passen. Daarnaast kunnen klanten de service 30 dagen uitproberen en deze op elk moment annuleren zonder verplichtingen.</p>
-      <img src="../img/Homepagina/roof.jpg" alt="img2">
+      <img src="img/Homepagina/roof.jpg" alt="img2">
     </div>
     <div class="columnhp3">
       <!-- Content for the third column -->
