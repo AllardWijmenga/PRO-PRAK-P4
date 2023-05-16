022625457fcea7f84cbbad9d7103701f7e7f2545
Amine Azzamouri <amine.azzamouri@gamil.com>
2023-05-16 08:51:09 +0200

    aboutpage added gif

--- aboutus.php
+++ aboutus.php
@@ -1,27 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
 </head>
+
 <body>
-<body>
-<header>
-    <nav class="nav-bar">
-      <div class="container">
-      <a href="index.php" class="logo"><img src="img/sl5.png" alt="Logo"></a>
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="product.php">Product</a></li>
-          <li><a href="contact.php">Contact</a></li>
-          <li><a href="aboutus.php">About us</a></li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-    
-</body>
+
+  <body class="aboutus-background-gif">
+    <header>
+      <nav class="nav-bar">
+        <div class="container">
+          <a href="index.php" class="logo"><img src="img/sl5.png" alt="Logo"></a>
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="product.php">Product</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="aboutus.php">About us</a></li>
+          </ul>
+        </div>
+      </nav>
+
+
+      <div class="footer-aboutp"></div>
+    </header>
+
+
+
+  </body>
+
 </html>
